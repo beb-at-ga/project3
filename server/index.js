7239d74fad378bb16df1647cf7ba450fbb71cb06
larynxmith <larynxmith@gmail.com>
2019-09-13 13:18:28 -0700

    changed index.js, added user.js model

--- server/index.js
+++ server/index.js
@@ -1,25 +1,39 @@
-// required
-let express = require('express')
+//required
+require('dotenv').config()
 let cors = require('cors')
+let express = require('express')
+let expressJwt = require('express-jwt')
+let morgan = require('morgan')
+let rowdyLogger = require('rowdy-logger')
 
 //instance
 let app = express()
+let rowdyResults = rowdyLogger.begin(app)
 
 //middleware
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json({ limit: '50mb' }))
 app.use(cors())
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json({ limit: '10mb' }))
 
-// controllers
 
+// routes
+app.use('/auth', expressJwt({
+    secret: process.env.JWT_SECRET
+}).unless({
+    path: [
+        { url: '/auth/login', methods: ['POST'] },
+        { url: '/auth/signup', methods: ['POST'] }
+    ]
+}), require('./controllers/auth'))
 
-//routes
+//Catch-All route
 app.get('*', (req, res) => {
     res.status(404).send({ message: 'Not Found' })
 })
 
 
-// listen
 app.listen(process.env.PORT || 3000, () => {
     console.log('Hear, Here!')
+    rowdyResults.print()
 })
