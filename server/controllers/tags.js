4c7a9ab70dc41bbc8464f6490cf1217a61a001fc
larynxmith <larynxmith@gmail.com>
2019-09-13 14:32:36 -0700

    working on new controllers

---
+++ server/controllers/tags.js
@@ -0,0 +1,26 @@
+// create router and reference to models
+let router = require('express').Router()
+let db = require('../../models')
+
+
+//POST to /tags
+router.post('/', (req, res) => {
+    db.Tag.create(req.body)
+    .then(tag => {
+        res.status(201).send(tag)
+    })
+})
+
+//PUT to /tags/:id
+router.put(':id', (req, res) => {
+    db.Tag.findOneandUpdate({
+        _id: req.params.id
+    })
+    .then(editedTag => {
+        res.send(editedTag)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(503).send({ message: 'Server Error'})
+    })
+})
