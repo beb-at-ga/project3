13dd587ae0726d820d43216b9a26eb7555c10ece
Bryan Brinson <bryan@beb.com>
2019-09-16 13:19:55 -0700

    some todo on the home page

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 const Home = (props) => {
 
-
   if (!props.user._id) {
     return (
       <div>
-        Unauthenticated Home
+        <p>Unauthenticated Home</p>
+        <ul>
+          <li>Add Hero images (maybe 2 or 3)</li>
+          <li>Add some content about what it is.</li>
+          <li>Add some content about how to get started.</li>
+          <li>Maybe put a Signup button on in the call to action.</li>
+        </ul>
       </div>
     )
   } else {
