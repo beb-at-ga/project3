b510e5b8122389d6700498b9587bfb926d899f91
larynxmith <larynxmith@gmail.com>
2019-09-16 13:57:42 -0700

    Footer STUBS written

---
+++ client/src/components/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Us: </h1>
+            <h2></h2>
+        </div>
+    )
+}
+
+export default About
