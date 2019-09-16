c40d3765b31333fb78925fb2ed8f2deb66abe100
Bryan Brinson <bryan@beb.com>
2019-09-16 14:11:00 -0700

    test profiles routes: get /:id, put /:id, delete /:id

--- server/index.js
+++ server/index.js
@@ -1,39 +1,56 @@
 //required
-require('dotenv').config()
-let cors = require('cors')
-let express = require('express')
-let expressJwt = require('express-jwt')
-let morgan = require('morgan')
-let rowdyLogger = require('rowdy-logger')
+require("dotenv").config();
+let cors = require("cors");
+let express = require("express");
+let expressJwt = require("express-jwt");
+let morgan = require("morgan");
+let rowdyLogger = require("rowdy-logger");
 
 //instance
-let app = express()
-let rowdyResults = rowdyLogger.begin(app)
+let app = express();
+let rowdyResults = rowdyLogger.begin(app);
 
 //middleware
-app.use(cors())
-app.use(morgan('dev'))
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json({ limit: '10mb' }))
-
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "10mb" }));
 
 // routes
-app.use('/auth', expressJwt({
+app.use(
+  "/auth",
+  expressJwt({
     secret: process.env.JWT_SECRET
-}).unless({
+  }).unless({
     path: [
-        { url: '/auth/login', methods: ['POST'] },
-        { url: '/auth/signup', methods: ['POST'] }
+      { url: "/auth/login", methods: ["POST"] },
+      { url: "/auth/signup", methods: ["POST"] }
     ]
-}), require('./controllers/auth'))
+  }),
+  require("./controllers/auth")
+);
+
+
+app.use(
+  "/profiles",
+  expressJwt({
+    secret: process.env.JWT_SECRET
+  }).unless({
+    path: [
+      { url: "/auth/login", methods: ["POST"] },
+      { url: "/auth/signup", methods: ["POST"] }
+    ]
+  }),
+  require("./controllers/profiles")
+);
+
 
 //Catch-All route
-app.get('*', (req, res) => {
-    res.status(404).send({ message: 'Not Found' })
-})
-
+app.get("*", (req, res) => {
+  res.status(404).send({ message: "Not Found" });
+});
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('Hear, Here!')
-    rowdyResults.print()
-})
+  console.log("Hear, Here!");
+  rowdyResults.print();
+});
