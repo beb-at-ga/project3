3c4979e28c31831a22709af41d0cea518a2c7daf
Bryan Brinson <bryan@beb.com>
2019-09-17 14:22:09 -0700

    stripped out a bunch of comments

--- client/src/components/pages/profile/ProfileHeaderEdit.jsx
+++ client/src/components/pages/profile/ProfileHeaderEdit.jsx
@@ -4,42 +4,16 @@ import axios from 'axios';
 
 // Material-UI Components
 import Button from '@material-ui/core/Button';
-// import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-// import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
-
-// const useStyles = makeStyles(theme => ({
-//   paper: {
-//     position: 'absolute',
-//     width: '90%',
-//     height: '50vh',
-//     backgroundColor: theme.palette.background.paper,
-//     border: '0px solid #000',
-//     padding: theme.spacing(2, 4, 3),
-//   },
-//   form: {
-//     display: 'flex',
-//     flexDirection: 'column',
-//     margin: 'auto',
-//     width: 'fit-content',
-//   },
-// }));
 
 const ProfileHeaderEdit = (props) => {
 
   const [open, setOpen] = React.useState(false);
-
-  // const [firstname, setFirstname] = useState('');
-  // const [lastname, setLastname] = useState('');
-  // const [email, setEmail] = useState('');
-  // const [menteeTag, setMenteeTag] = useState('');
-  // const [mentorTag, setMentorTag] = useState('');
-  // const [profilePic, setProfilePic] = useState('');
 
   function handleClickOpen() {
     setOpen(true);
@@ -73,37 +47,19 @@ const ProfileHeaderEdit = (props) => {
       profilePic: e.target.profilePic.value
     }
     
-    console.log(body);
-
     axios.put(`${BASE_URL}/profiles/${props.user._id}`, body, {
       headers: { 'Authorization': `Bearer ${localStorage.getItem('authToken')}` }
     })
       .then(response => {
         console.log(response);
-        // saving the new token so that the user's edits will be rendered
         localStorage.setItem('authToken', response.data.token);
         props.updateUser();
-
-        // Go back to the Bio component that just shows the text
-        // this.setState({
-        //   currentTab: 'bio'
-        // })
       })
-
   }
 
   function handleClose() {
     setOpen(false);
   }
-
-  // let profilePic = '';
-  // if (props.user.profilePic) {
-  //   profilePic = <img src={props.user.profilePic} alt='profile'></img>
-  // } else {
-  //   profilePic = <img src='http://placegoat.com/100/100' alt='profile'></img>
-  // }
-
-  // let test = 'fdfd';
 
   return (
     <div>
@@ -145,38 +101,8 @@ const ProfileHeaderEdit = (props) => {
 
         </DialogContent>
       </Dialog>
-
-
-      {/* <Modal open={open} onClose={handleClose}>
-        <div style={modalStyle} className={classes.paper}>
-          <form type='submit' onSubmit={saveAndClose} className={classes.form}>
-            <input name='firstname' defaultdefaultValue={props.user.firstname}>First Name:</input>
-            <input name='lastname' defaultdefaultValue={props.user.lastname}>Last Name:</input>
-            <input name='email' defaultdefaultValue={props.user.email}>Email:</input>
-            <input name='menteeTag' defaultdefaultValue={props.user.menteeTag}>Mentee Tags:</input>
-            <input name='mentorTag' defaultdefaultValue={props.user.mentorTag}>Mentor Tags:</input>
-            <button type='submit' onClick={handleClose} >Save</button>
-          </form>
-        </div>
-      </Modal> */}
-
     </div>
-    //   <Box >
-    //     <h1>{props.user.firstname} {props.user.lastname}</h1>
-    //     <Grid container spacing={3}>
-    //       <Grid item xs >
-    //         {profilePic}
-    //       </Grid>
-    //       <Grid item xs>
-    //         <p>{props.user.email}</p>
-    //       </Grid>
-    //       <Grid item xs={6}>
-    //         <a href='htts://www.google.com'>Google</a>
-    //       </Grid>
-    //     </Grid>
-    //   </Box>
   )
 }
 
 export default ProfileHeaderEdit;
-
