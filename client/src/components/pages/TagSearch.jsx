d66c6ade64aa95cf1db870ed9440e0448537f773
larynxmith <larynxmith@gmail.com>
2019-09-18 12:14:14 -0700

    TagSearch publishes to Profile

--- client/src/components/pages/TagSearch.jsx
+++ client/src/components/pages/TagSearch.jsx
@@ -7,7 +7,10 @@ import BASE_URL from '../../constants';
 class  TagSearch extends React.Component {
 
     state = {
-        tagsInput: ''
+        tagsInput: '',
+        mentors: [],
+        mentees: []
+
     }
 
     handleChange = (e) => {
@@ -27,19 +30,62 @@ class  TagSearch extends React.Component {
                 headers: { 'Authorization': `Bearer ${token}` }
             })
         .then(response => {
-            console.log('SUCCESS', response.data)
+            console.log('SUCCESS', response.data.foundUsers)
+            let mentors = []
+            let mentees = []
+            
+            console.log(trimmedStrings)
+            response.data.foundUsers.forEach(user => {
+                // checking for an intersection of the searched tag within the user's mentorTag array
+                let intersectedTags = user.mentorTag.filter(tags => trimmedStrings.includes(tags))
+                // if present, push user into mentors array
+                if(intersectedTags.length) {
+                    console.log('Adding to mentors: ', intersectedTags, user.mentorTag)
+                    mentors.push(user)
+                }
+                // then repeat process for mentees array
+                intersectedTags = user.menteeTag.filter(tags => trimmedStrings.includes(tags))
+                if (intersectedTags.length) {
+                    mentees.push(user)
+                }
+            })
+            this.setState({ mentors: mentors, mentees: mentees })
         })
     }
     render () {
+        let mentorsList = (this.state.mentors.map((m, i) => {
+            return (
+                <ul key={i}>
+                    <li>
+                        {m.firstname} {m.lastname} -- {m.mentorTag.join(', ')}
+                    </li>
+                </ul>
+            )
+        }))
+
+        let menteesList = (this.state.mentees.map((m, i) => {
+            return (
+                <ul key={i}>
+                    <li>
+                        {m.firstname} {m.lastname} -- {m.menteeTag.join(', ')}
+                    </li>
+                </ul>
+            )
+        }))
+
     return(
         <div>
             <form onSubmit={this.searchList}>
-                <label for="tag-search">Tag Search:</label>
-                <input type="text" name="tag-search" value={this.state.tagsInput} onChange={this.handleChange}/>
-                <button type="submit" />
+                <label htmlFor="tag-search">Tag Search:</label>
+                <input type="text" name="tag-search" value={this.state.tagsInput} onChange={this.handleChange} />
+                <button type="submit">Search Tags</button>
             </form>
             <hr />
-            <p>{this.searchList}</p>
+            <h3>Here is a List of Mentors: </h3>
+            <h4>{mentorsList}</h4>
+            <h3>Here is a List of Other Mentees: </h3>
+            <h4>{menteesList}</h4>
+            <hr />
         </div>
         )
     }
