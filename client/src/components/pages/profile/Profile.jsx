5759cc5ed0c1517ac07bd7769ecce032dc490466
larynxmith <larynxmith@gmail.com>
2019-09-18 13:45:22 -0700

    TagSearch moved to separate column in Profile

--- client/src/components/pages/profile/Profile.jsx
+++ client/src/components/pages/profile/Profile.jsx
@@ -4,6 +4,9 @@ import ProfileHeader from './ProfileHeader';
 import ProfileDetails from './ProfileDetails';
 import Tags from './Tags';
 import TagSearch from '../TagSearch';
+import { Grid } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+
 
 const Profile = (props) => {
 
@@ -13,13 +16,19 @@ const Profile = (props) => {
   }
 
   return (
-    <div>
-      <ProfileHeader user={props.user} updateUser={props.updateUser} />
-      <Tags user={props.user} />
-      {/* <ProfileDetails user={props.user}/> */}
-      <TagSearch />
-      <ProfileDetails user={props.user} updateUser={props.updateUser} />
-    </div>
+      <Box>
+        <Grid container spacing={3}>
+            <Grid item xs={6}>
+                <ProfileHeader user={props.user} updateUser={props.updateUser} />
+                <Tags user={props.user} />
+                {/* <ProfileDetails user={props.user}/> */}
+                <ProfileDetails user={props.user} updateUser={props.updateUser} />
+            </Grid>
+            <Grid item>
+                <TagSearch />
+            </Grid>
+        </Grid>
+      </Box>
   )
 }
 
