887151f7007d04d6db38df39fc791d69dfd8e38a
larynxmith <larynxmith@gmail.com>
2019-09-18 15:10:29 -0700

    some mat-ui styling

--- client/src/components/pages/profile/Profile.jsx
+++ client/src/components/pages/profile/Profile.jsx
@@ -6,39 +6,35 @@ import Tags from './Tags';
 import TagSearch from '../TagSearch';
 import { Grid } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
-import Paper from '@material-ui/core/Paper';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 
 
 
 
 const Profile = (props) => {
 
-  // this !props.user isn't currently working. The <Profile /> is still being rendered.
-  if (!props.user) {
-    return <Redirect to='/' />
-  }
+    // this !props.user isn't currently working. The <Profile /> is still being rendered.
+    if (!props.user) {
+        return <Redirect to='/' />
+    }
 
-  return (
-      <Box>
-        <Grid container spacing={3}>
-            <Grid item xs={6}>
-                <ProfileHeader user={props.user} updateUser={props.updateUser} />
-                <Tags user={props.user} />
-                {/* <ProfileDetails user={props.user}/> */}
-                <ProfileDetails user={props.user} updateUser={props.updateUser} />
+    return (
+        <Box>
+            <Grid container spacing={5}>
+                <Grid item xs={6}>
+                    <ProfileHeader user={props.user} updateUser={props.updateUser} />
+                    <Tags user={props.user} />
+                    {/* <ProfileDetails user={props.user}/> */}
+                    <ProfileDetails user={props.user} updateUser={props.updateUser} />
+                </Grid>
+                <Grid item >
+                    <Card>
+                        <TagSearch />
+                    </Card>
+                </Grid>
             </Grid>
-            <Grid item>
-                <Card>
-                    <TagSearch />
-                  </Card>
-            </Grid>
-        </Grid>
-      </Box>
-  )
+        </Box>
+    )
 }
 
 export default Profile;
