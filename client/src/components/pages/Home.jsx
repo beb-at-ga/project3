b482c8d1f2046f9d3469b782280a86a67485bbfe
michelleurrea <michelleurrea@gmail.com>
2019-09-18 16:57:17 -0700

    Authorized home page has content now

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,6 +1,29 @@
 import React from 'react';
+import TagSearch from './TagSearch'
+import "../../App.css";
 
-const Home = (props) => {
+// Material-Ui
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles({
+  bigAvatar: {
+    margin: 10,
+    width: 120,
+    height: 120,
+  },
+});
+
+export default function ImageAvatars(props) {
+  const classes = useStyles();
+
+  let profilePic = '';
+  if (props.user.profilePic) {
+    profilePic = <img src={props.user.profilePic} alt='profile'></img>
+  } else {
+    profilePic = <img src='http://placegoat.com/100/100' alt='profile'></img>
+  }
 
   if (!props.user._id) {
     return (
@@ -16,12 +39,24 @@ const Home = (props) => {
       </div>
     )
   } else {
-    return (
-      <div>
-        Authenticated Home
-      </div>
-    )
+      return (
+        <div className="home">
+          <Grid container justify="center" alignItems="center">
+            <h1>Welcome Back...</h1>
+          </Grid>
+          <Grid container justify="center" alignItems="center">
+            <Avatar alt="Remy Sharp" src={props.user.profilePic} className={classes.bigAvatar} />
+          </Grid>
+          <Grid container justify="center" alignItems="center">
+            <h2>{props.user.firstname}</h2>
+          </Grid>
+          <Grid container justify="center" alignItems="center">
+            <p>Start your search here</p>
+          </Grid>
+          <Grid container justify="center" alignItems="center">
+            <TagSearch />
+          </Grid>
+        </div>
+     )
   }
 }
-
-export default Home;
